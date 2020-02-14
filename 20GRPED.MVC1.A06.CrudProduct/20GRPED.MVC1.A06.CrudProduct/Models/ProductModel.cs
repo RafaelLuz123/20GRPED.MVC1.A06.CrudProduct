c641cc2b73837230ab2ef6752b70227673e1dca4
Felipe Andrade <villela.felipe@gmail.com>
2020-02-14 07:32:38 -0300

    Added Model, Controller and Views

---
+++ 20GRPED.MVC1.A06.CrudProduct/20GRPED.MVC1.A06.CrudProduct/Models/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _20GRPED.MVC1.A06.CrudProduct.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
